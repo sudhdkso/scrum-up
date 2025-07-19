91f75464ed8efe8d090cf3aa07b94d72bce24964
원지윤 <waga1009@gmail.com>
2025-07-19 18:27:47 +0900

    fix: 오류 수정

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,10 +1,13 @@
 import dbConnect from "@/lib/mongodb";
 import User from "@/models/user";
 
-export async function GET(req: Request, context: { params: { id: string } }) {
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
     await dbConnect();
-    const id = context.params.id;
+    const id = (await params).id;
     const user = await User.findById(id);
     if (!user) {
       return new Response("User not found", { status: 400 });
