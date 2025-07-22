147dfc5f4bdb9d66d624b88d9e38f45d14c10c05
원지윤 <waga1009@gmail.com>
2025-07-22 18:57:20 +0900

    style: 버튼 스타일 변경

---
+++ src/models/group-member.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IGroupMember extends Document {
+  _id: string;
+  groupId: string;
+  userId: string;
+  name: string;
+  role?: string;
+  joinedAt?: Date;
+}
+
+const GroupMemberSchema: Schema = new Schema({
+  groupId: { type: Schema.Types.ObjectId, ref: "Group", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  role: { type: String, default: "member" },
+  joinedAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.GroupMember ||
+  mongoose.model<IGroupMember>("GroupMember", GroupMemberSchema);
