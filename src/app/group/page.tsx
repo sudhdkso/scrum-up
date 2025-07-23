28cade02e45e6937f13b59502546f4eb73734d75
원지윤 <waga1009@gmail.com>
2025-07-23 17:54:21 +0900

    fix:css 경로 수정

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import styles from "../style/groupForm.module.css";
+import styles from "@/style/groupForm.module.css";
 import Button from "../../components/Button";
 import TextInput from "../../components/TextInput";
 import TextAreaInput from "../../components/TextAreaInput";
